8819eb7831d2bb778be25f603db8a50acf9533a5
Sridhar <94095513+Sri-dhar@users.noreply.github.com>
2024-01-23 16:12:08 +0000

    error fixed ft>Viraj

--- file1.c
+++ file1.c
@@ -4,134 +4,187 @@
 #include <sys/types.h>
 #include <fcntl.h>
 #include <sys/wait.h>
+#include <string.h>
 #include <ctype.h>
 
-void count() {
-    int count = 0;
-    int ch;
+#define LSH_RL_BUFSIZE 1024
 
-    while ((ch = getchar()) != EOF) {
-        if (!isalpha(ch)) {
-            count++;
-        }
-        putchar(ch);
-    }
+// Function declarations
+char *lsh_read_line(void);
+char **lsh_split_line(char *line);
+int lsh_execute(char **args);
+int lsh_launch(char **args);
+void lsh_loop(void);
 
-    fprintf(stderr, "Count of non alphabetic characters: %d\n", count);
-    exit(0);
+// Built-in command declarations
+int lsh_cd(char **args);
+int lsh_help(char **args);
+int lsh_exit(char **args);
+
+// Array of built-in command strings
+char *builtin_str[] = {
+    "cd",
+    "help",
+    "exit"
+};
+
+// Array of built-in command functions
+int (*builtin_func[])(char **) = {
+    &lsh_cd,
+    &lsh_help,
+    &lsh_exit
+};
+
+int lsh_num_builtins() {
+    return sizeof(builtin_str) / sizeof(char *);
 }
 
-void convert() {
-    int ch;
+char *lsh_read_line(void) {
+    int bufsize = LSH_RL_BUFSIZE;
+    int position = 0;
+    char *buffer = malloc(sizeof(char) * bufsize);
+    int c;
 
-    while ((ch = getchar()) != EOF) {
-        if (islower(ch)) {
-            putchar(toupper(ch));
-        } else if (isupper(ch)) {
-            putchar(tolower(ch));
+    if (!buffer) {
+        fprintf(stderr, "lsh: allocation error\n");
+        exit(EXIT_FAILURE);
+    }
+
+    while (1) {
+        c = getchar();
+
+        if (c == EOF || c == '\n') {
+            buffer[position] = '\0';
+            return buffer;
         } else {
-            putchar(ch);
+            buffer[position] = c;
+        }
+        position++;
+
+        if (position >= bufsize) {
+            bufsize += LSH_RL_BUFSIZE;
+            buffer = realloc(buffer, bufsize);
+            if (!buffer) {
+                fprintf(stderr, "lsh: allocation error\n");
+                exit(EXIT_FAILURE);
+            }
+        }
+    }
+}
+
+char **lsh_split_line(char *line) {
+    int bufsize = LSH_RL_BUFSIZE;
+    int position = 0;
+    char **tokens = malloc(bufsize * sizeof(char *));
+    char *token;
+
+    if (!tokens) {
+        fprintf(stderr, "lsh: allocation error\n");
+        exit(EXIT_FAILURE);
+    }
+
+    token = strtok(line, " \t\r\n\a");
+    while (token != NULL) {
+        tokens[position] = token;
+        position++;
+
+        if (position >= bufsize) {
+            bufsize += LSH_RL_BUFSIZE;
+            tokens = realloc(tokens, bufsize * sizeof(char *));
+            if (!tokens) {
+                fprintf(stderr, "lsh: allocation error\n");
+                exit(EXIT_FAILURE);
+            }
+        }
+
+        token = strtok(NULL, " \t\r\n\a");
+    }
+    tokens[position] = NULL;
+    return tokens;
+}
+
+int lsh_cd(char **args) {
+    if (args[1] == NULL) {
+        fprintf(stderr, "lsh: expected argument to \"cd\"\n");
+    } else {
+        if (chdir(args[1]) != 0) {
+            perror("lsh");
+        }
+    }
+    return 1;
+}
+
+int lsh_help(char **args) {
+    printf("Simple Shell\n");
+    printf("Enter program names and arguments, and press enter.\n");
+    printf("Built-in commands:\n");
+    printf("  cd <directory>: Change the current working directory\n");
+    printf("  help: Display this help message\n");
+    printf("  exit: Exit the shell\n");
+    return 1;
+}
+
+int lsh_exit(char **args) {
+    return 0;
+}
+
+int lsh_execute(char **args) {
+    int i;
+
+    if (args[0] == NULL) {
+        return 1;
+    }
+
+    for (i = 0; i < lsh_num_builtins(); i++) {
+        if (strcmp(args[0], builtin_str[i]) == 0) {
+            return (*builtin_func[i])(args);
         }
     }
 
-    exit(0);
+    return lsh_launch(args);
 }
 
-int main(int argc, char *argv[]) {
-    int fd_in, fd_out;
-    int pipe_fd[2];
-    pid_t pid_count, pid_convert;
+int lsh_launch(char **args) {
+    pid_t pid, wpid;
     int status;
 
-    if (argc != 3) {
-        fprintf(stderr, "Usage: %s file1 file2\n", argv[0]);
-        exit(1);
-    }
-
-    fd_in = open(argv[1], O_RDONLY);
-    if (fd_in == -1) {
-        perror("Error opening file1");
-        exit(1);
-    }
-
-    fd_out = creat(argv[2], 0644);
-    if (fd_out == -1) {
-        perror("Error creating file2");
-        close(fd_in);
-        fd_out = creat(argv[2], 0644);
-        exit(1);
-    }
-
-    close(0);
-    if (dup(fd_in) == -1) {
-        perror("Error duplicating file descriptor for stdin");
-        close(fd_in);
-        close(fd_out);
-        exit(1);
-    }
-
-    close(1);
-    if (dup(fd_out) == -1) {
-        perror("Error duplicating file descriptor for stdout");
-        close(fd_in);
-        close(fd_out);
-        exit(1);
-    }
-
-    if (pipe(pipe_fd) == -1) {
-        perror("Error creating pipe");
-        close(fd_in);
-        close(fd_out);
-        exit(1);
-    }
-
-    pid_count = fork();
-    if (pid_count == -1) {
-        perror("Error forking count");
-        close(fd_in);
-        close(fd_out);
-        close(pipe_fd[0]);
-        close(pipe_fd[1]);
-        exit(1);
-    }
-
-    if (pid_count == 0) {
-        close(pipe_fd[1]);
-        close(0);
-        dup(pipe_fd[0]);
-        close(pipe_fd[0]);
-        close(fd_in);
-        close(fd_out);
-        count();
+    pid = fork();
+    if (pid == 0) {
+        // Child process
+        if (execvp(args[0], args) == -1) {
+            perror("lsh");
+        }
+        exit(EXIT_FAILURE);
+    } else if (pid < 0) {
+        // Error forking
+        perror("lsh");
     } else {
-        pid_convert = fork();
-        if (pid_convert == -1) {
-            perror("Error forking convert");
-            close(fd_in);
-            close(fd_out);
-            close(pipe_fd[0]);
-            close(pipe_fd[1]);
-            waitpid(pid_count, &status, 0);
-            exit(1);
-        }
-
-        if (pid_convert == 0) {
-            close(pipe_fd[0]);
-            close(1);
-            dup(pipe_fd[1]);
-            close(pipe_fd[1]);
-            close(fd_in);
-            close(fd_out);
-            convert();
-        } 
-        else {
-            close(pipe_fd[0]);
-            close(pipe_fd[1]);
-            waitpid(pid_count, &status, 0);
-            waitpid(pid_convert, &status, 0);
-            close(fd_in);
-            close(fd_out);
-            exit(0);
-        }
+        // Parent process
+        do {
+            wpid = waitpid(pid, &status, WUNTRACED);
+        } while (!WIFEXITED(status) && !WIFSIGNALED(status));
     }
+
+    return 1;
+}
+
+void lsh_loop(void) {
+    char *line;
+    char **args;
+    int status;
+
+    do {
+        printf("> ");
+        line = lsh_read_line();
+        args = lsh_split_line(line);
+        status = lsh_execute(args);
+
+        free(line);
+        free(args);
+    } while (status);
+}
+
+int main(int argc, char **argv) {
+    lsh_loop();
+    return EXIT_SUCCESS;
 }
